f193f8ff3e4fe94563921526f667b5a5c6e8e2a7
unknown <ihzhanauvalb@gmail.com>
2024-06-23 00:56:52 +0700

    ...

--- client/src/pages/createBlog/Menubar.tsx
+++ client/src/pages/createBlog/Menubar.tsx
@@ -1,5 +1,16 @@
 import { ToggleGroup, ToggleGroupItem } from "@radix-ui/react-toggle-group";
 import { Editor } from "@tiptap/react";
+import { FaBold, FaItalic, FaUnderline } from "react-icons/fa";
+import { PiTextTBold } from "react-icons/pi";
+import { MdFormatListBulleted } from "react-icons/md";
+import { AiOutlineOrderedList } from "react-icons/ai";
+import {
+  LuHeading1,
+  LuHeading2,
+  LuHeading3,
+  LuHeading4,
+  LuHeading5,
+} from "react-icons/lu";
 
 interface Props {
   editor: Editor;
@@ -14,8 +25,101 @@ export default function Menubar({ editor }: Props) {
       <ToggleGroupItem
         value="bold"
         onClick={() => editor.chain().focus().toggleBold().run()}
-        className={editor.isActive("bold") ? "bg-slate-300" : ""}
-      ></ToggleGroupItem>
+        active={editor.isActive("bold")}
+      >
+        <FaBold />
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="italic"
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        active={editor.isActive("italic")}
+      >
+        <FaItalic />
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="underline"
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        active={editor.isActive("underline")}
+      >
+        <FaUnderline />
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="paragraph"
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        active={editor.isActive("paragraph")}
+      >
+        <PiTextTBold />
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="bold"
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        active={editor.isActive("paragraph")}
+      >
+        <PiTextTBold />
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="heading1"
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        active={editor.isActive("heading", { level: 1 })}
+      >
+        <LuHeading1 />
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="heading2"
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        active={editor.isActive("heading", { level: 2 })}
+      >
+        <LuHeading2 />
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="heading1"
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        active={editor.isActive("heading", { level: 1 })}
+      >
+        <LuHeading1 />
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="heading3"
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        active={editor.isActive("heading", { level: 3 })}
+      >
+        <LuHeading3 />
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="heading4"
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        active={editor.isActive("heading", { level: 4 })}
+      >
+        <LuHeading4 />
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="heading5"
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        active={editor.isActive("heading", { level: 5 })}
+      >
+        <LuHeading5 />
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="heading6"
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        active={editor.isActive("heading", { level: 6 })}
+      >
+        <LuHeading5 />
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="unorderd_list"
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        active={editor.isActive("bulletList")}
+      >
+        <MdFormatListBulleted />
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="ordered_list"
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        active={editor.isActive("orderedList")}
+      >
+        <AiOutlineOrderedList />
+      </ToggleGroupItem>
     </ToggleGroup>
   );
 }
